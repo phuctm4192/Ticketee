753e5ad96f2c26ac105f4defcceb839c9d917758
Thai Phuc <phuc.tm4192@gmail.com>
2018-05-20 15:14:07 +0700

    Provide an 'Add another file' link that uses JavaScript so that users can upload more than one file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
-//= require turbolinks
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,3 +58,9 @@ RSpec.configure do |config|
   config.include Warden::Test::Helpers, type: :feature
   config.after(type: :feature) { Warden.test_reset! }
 end
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.javascript_driver = :chrome
