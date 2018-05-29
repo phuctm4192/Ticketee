804a263337606570c08c256795fffbcfce990289
Thai Phuc <phuc.tm4192@gmail.com>
2018-05-29 20:11:08 +0700

    Tickets can have a state assigned when comments are created

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,18 @@
 class Comment < ApplicationRecord
   belongs_to :ticket
   belongs_to :author, class_name: "User"
+  belongs_to :state
   delegate :project, to: :ticket
 
   validates :text, presence: true
 
   scope :persisted, lambda { where.not(id: nil) }
+
+  after_create :set_ticket_state
+
+  private
+  def set_ticket_state
+    ticket.state = state
+    ticket.save!
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.active_record.belongs_to_required_by_default = true
 end
