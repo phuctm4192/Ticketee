77cf6244e533fabea161d712796a6c099655dd9d
Thai Phuc <phuc.tm4192@gmail.com>
2018-06-05 15:42:15 +0700

    Admins can now set a default state for tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180529141448) do
+ActiveRecord::Schema.define(version: 20180605082522) do
 
   create_table "attachments", force: :cascade do |t|
     t.string "file"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180529141448) do
     t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "default", default: false
   end
 
   create_table "tickets", force: :cascade do |t|
